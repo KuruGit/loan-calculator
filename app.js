8f09f7ca8f5534ec3ff16cd077ec878621703d72
David <david.breier@outlook.de>
2018-04-25 18:12:35 +0200

    Error handling function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Listen for submit button
 document.getElementById('loan-form').addEventListener('submit', calculateResults);
-
+function calculateResults(e){
 //UI Vars
 const UIamount = document.getElementById('amount');
 const UIinterest = document.getElementById('interest');
@@ -23,11 +23,33 @@ if(isFinite(monthly)){
     UItotalPayment.value = (monthly*calculatedPayments).toFixed(2);
     UItotalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
 } else {
-    console.log("Bitte Zahlen checken!");
+    showError("Bitte prüfen sie ihre Eingabe!");
 }
 
-//Calculate Results
-function calculateResults(){
-    alert("Klickedicklick");
+e.preventDefault();
 }
 
+//Error Handling Function
+
+function showError(error){
+    //create a div
+    const errorDiv = document.createElement('div');
+    //get Elements to sort errorDiv into DOM
+    const card = document.querySelector(".card");
+    const heading = document.querySelector(".heading");
+    //add Bootstrap class to make Div dangerous-red 
+    errorDiv.className = "alert alert-danger";
+    //add Textnode to errorDiv with error text from if/else statement to check if monthly is finite
+    errorDiv.appendChild(document.createTextNode(error));
+    //insert error message before heading
+    card.insertBefore(errorDiv, heading);
+
+    //clear error after set time
+
+    setTimeout(clearError, 2000);
+}
+//TODO:Timeout for the showError function. SetTimeout first test did not work. Problem: with every click another error message is generated
+//Clear error
+function clearError(){
+    document.querySelector(".alert").remove();
+}
